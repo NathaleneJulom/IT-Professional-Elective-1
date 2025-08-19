c9a06903c72c25570505f20233a3d6b125c4f5fa
Nathalene S. Julom <julomacads@gmail.com>
2025-08-19 10:23:32 +0800

    Create Activity1.java

---
+++ Activity 1.java
@@ -0,0 +1,43 @@
+//Julom, Nathalene S.
+//Activity 1: Basic Math with Condition
+
+class BasicMath {
+    public static void main(String[] args) {
+        //Initialize operands and operator
+        int num1 = 5, num2 = 5;
+        char op = '*';
+        
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Operation (+ - * /): " + op);
+        
+        double answer;
+        
+        switch (op) {
+            case '+':
+                answer = num1 + num2;
+                System.out.println("The sum of " + num1 + " and " + num2 + " is " + answer);
+                break;
+            case '-':
+                answer = num1 - num2;
+                System.out.println("The difference of " + num1 + " and " + num2 + " is " + answer);
+                break;
+            case '*':
+                answer = num1 * num2;
+                System.out.println("The product of " + num1 + " and " + num2 + " is " + answer);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    answer = num1 / num2;
+                    System.out.println("The quotient of " + num1 + " and " + num2 + " is " + answer);
+                }
+                else {
+                    System.out.println("Error: Can't divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                break;
+        }
+    }
+}
